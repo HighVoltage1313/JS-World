f8fae915014f24de4211359b62525f7e9fffb0a2
Alehander <shyrik1331@gmail.com>
2024-09-16 13:10:52 +0300

    Style changes
    Added backgroung gradient
    Removed border radius

--- script.js
+++ script.js
@@ -4,10 +4,9 @@ document.addEventListener('DOMContentLoaded', function() {
 
     buttons.forEach((button, index) => {
         button.addEventListener('click', function() {
-            // Удаляем класс active со всех страниц
+
             pages.forEach(page => page.classList.remove('active'));
 
-            // Добавляем класс active только на соответствующую страницу
             pages[index].classList.add('active');
         });
     });
