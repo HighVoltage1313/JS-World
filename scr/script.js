5e0cf7737501a528b35018230b18a8af7761a1dc
Alehander <shyrik1331@gmail.com>
2024-10-02 00:51:36 +0300

    Updated Basic Page
    Added menu buttons
    Added page changing
    Added button animation
    +other minor fixes

--- scr/script.js
+++ scr/script.js
@@ -1,14 +1,21 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const buttons = document.querySelectorAll('.menuItem button');
     const pages = document.querySelectorAll('.contentPage');
+    const buttons = document.querySelectorAll('.container__pages .page');
 
-    buttons.forEach((button, index) => {
-        button.addEventListener('click', function() {
+    const buttonArr = buttons;
 
-            pages.forEach(page => page.classList.remove('active'));
-
-            pages[index].classList.add('active');
-        });
+    pages.forEach((page, index) => {
+        let search = page.classList.contains(window.location.search.substring(1));  
+        if (search) {page.classList.add('active'); buttonArr[index].classList.add('active');}
+        else {page.classList.remove('active'); buttonArr[index].classList.remove('active');}     
     });
+
 });
+
+
+
+
+
+
+
 
