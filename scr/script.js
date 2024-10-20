45a5da36c2e1d44e8ba70fd124b5db0954999dd9
Alehander <shyrik1331@gmail.com>
2024-10-20 18:04:39 +0300

    Minor Fixes
    Added hash tags for buttons
    Code formating

--- scr/script.js
+++ scr/script.js
@@ -1,21 +1,18 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const pages = document.querySelectorAll('.contentPage');
-    const buttons = document.querySelectorAll('.container__pages .page');
+window.addEventListener("DOMContentLoaded", updateContent);
+window.addEventListener("hashchange", updateContent);
 
-    const buttonArr = buttons;
+function updateContent() {
+  const pages = document.querySelectorAll(".contentPage");
+  const buttons = document.querySelectorAll(".container__pages .page");
 
-    pages.forEach((page, index) => {
-        let search = page.classList.contains(window.location.search.substring(1));  
-        if (search) {page.classList.add('active'); buttonArr[index].classList.add('active');}
-        else {page.classList.remove('active'); buttonArr[index].classList.remove('active');}     
-    });
-
-});
-
-
-
-
-
-
-
-
+  pages.forEach((page, index) => {
+    const search = page.classList.contains(window.location.hash.substring(1));
+    if (search) {
+      page.classList.add("active");
+      buttons[index].classList.add("active");
+    } else {
+      page.classList.remove("active");
+      buttons[index].classList.remove("active");
+    }
+  });
+}
